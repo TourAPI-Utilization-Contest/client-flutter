92756688d9206b76e22233d155db4566be625ebd
Tyndall-log <Tyndall-log@users.noreply.github.com>
2024-10-12 13:20:13 +0000

    deploy: fe25e38c30e9a5a49bd3cbc7fcc842352e42b241

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"059e4e6d8ff6de39c29441c53e949bfb0bf179
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3633061528"
+    serviceWorkerVersion: "2779670549"
   }
 });
